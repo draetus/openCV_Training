3eb160d5e7b83fdd7c339e90cf14fa6103b35258
Draetus <mauricio199836@hotmail.com>
2018-02-08 17:19:44 -0200

    'fixed' LBP_NON_Uniform implementation

--- LBP/LBP_Non_Uniform.cpp
+++ LBP/LBP_Non_Uniform.cpp
@@ -10,50 +10,54 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    cv::Mat image;
-    image = cv::imread(argv[1], 1);
+    cv::Mat original_image;
+    original_image = cv::imread(argv[1], 1);
 
-    if (!image.data)
+    cv::Mat LBP_image(original_image.rows, original_image.cols, CV_8UC1, cv::Scalar(0));
+    cv::Mat gray_image;
+    
+    
+    if (!original_image.data)
     {
         std::cout << "No image found...";
         return -1;
     }
 
-    cv::cvtColor(image, image, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(original_image, gray_image, cv::COLOR_BGR2GRAY);
 
     int pixelValue = 0;
 
     cv::namedWindow("Modified Pixel in Image", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Modified Pixel In Image", image);
+    cv::imshow("Modified Pixel In Image", original_image);
     cv::waitKeyEx(0);
    
-    for (int i=1; i<image.cols; i++)
+    for (int i=1; i<original_image.rows; i++)
     {
-        for (int j=1; j<image.rows; j++)
+        for (int j=1; j<original_image.cols; j++)
         {
             std::cout << "Column: " << i << "Row: " << j << std::endl;
 
-            pixelValue += (image.at<uchar>(i-1,j-1) <= image.at<uchar>(i,j)) ? 1 : 0;
-            pixelValue += (image.at<uchar>(i-1,j) <= image.at<uchar>(i,j)) ? 2 : 0;
-            pixelValue += (image.at<uchar>(i-1,j+1) <= image.at<uchar>(i,j)) ? 4 : 0;
-            pixelValue += (image.at<uchar>(i,j+1) <= image.at<uchar>(i,j)) ? 8 : 0;
-            pixelValue += (image.at<uchar>(i+1,j+1) <= image.at<uchar>(i,j)) ? 16 : 0;
-            pixelValue += (image.at<uchar>(i+1,j) <= image.at<uchar>(i,j)) ? 32 : 0;
-            pixelValue += (image.at<uchar>(i+1,j-1) <= image.at<uchar>(i,j)) ? 64 : 0;
-            pixelValue += (image.at<uchar>(i,j-1) <= image.at<uchar>(i,j)) ? 128 : 0;
+            pixelValue += (gray_image.at<uchar>(i-1,j-1) <= gray_image.at<uchar>(i,j)) ? 1 : 0;
+            pixelValue += (gray_image.at<uchar>(i-1,j) <= gray_image.at<uchar>(i,j)) ? 2 : 0;
+            pixelValue += (gray_image.at<uchar>(i-1,j+1) <= gray_image.at<uchar>(i,j)) ? 4 : 0;
+            pixelValue += (gray_image.at<uchar>(i,j+1) <= gray_image.at<uchar>(i,j)) ? 8 : 0;
+            pixelValue += (gray_image.at<uchar>(i+1,j+1) <= gray_image.at<uchar>(i,j)) ? 16 : 0;
+            pixelValue += (gray_image.at<uchar>(i+1,j) <= gray_image.at<uchar>(i,j)) ? 32 : 0;
+            pixelValue += (gray_image.at<uchar>(i+1,j-1) <= gray_image.at<uchar>(i,j)) ? 64 : 0;
+            pixelValue += (gray_image.at<uchar>(i,j-1) <= gray_image.at<uchar>(i,j)) ? 128 : 0;
+
             
-            image.at<uchar>(i,j) = pixelValue;
+            LBP_image.at<uchar>(i,j) = pixelValue;
             pixelValue = 0;
         }
-        cv::imshow("Modified Pixel In Image", image);
+        cv::imshow("Modified Pixel In Image", LBP_image);
         cv::waitKeyEx(1);
 
     }
     std::cout << pixelValue;
     
-    cv::imshow("Modified Pixel In Image", image);
+    cv::imshow("Modified Pixel In Image", LBP_image);
     cv::waitKeyEx(0);
-
 
     return 0;
 }
